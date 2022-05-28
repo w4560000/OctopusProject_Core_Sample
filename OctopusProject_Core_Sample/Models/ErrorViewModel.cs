7c1b805004ab613b8770227f337c9db52b16f641
w4560000 <39783609+w4560000@users.noreply.github.com>
2022-05-28 22:17:09 +0800

    加入專案檔案。

---
+++ OctopusProject_Core_Sample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OctopusProject_Core_Sample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
