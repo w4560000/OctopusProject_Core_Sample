9e57b71c133151d1239522d288889e61fea45483
w4560000 <39783609+w4560000@users.noreply.github.com>
2022-06-18 18:13:20 +0800

    codemaid

--- OctopusProject_Core_Sample/Program.cs
+++ OctopusProject_Core_Sample/Program.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using NLog;
 using NLog.Extensions.Logging;
 using NLog.Web;
 
